03f62a7a44a2314d506437f0feed330c7aef91df
briangitonga <bgritho@gmail.com>
2024-12-02 22:37:01 +0300

    added openpyxl

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import openpyxl
 import plotly.express as px
 import plotly.graph_objects as go
 from sklearn.linear_model import LinearRegression
